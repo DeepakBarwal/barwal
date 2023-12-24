1af618b98fe7f1612773cd276a4c7d07af7dee4d
DeepakBarwal <deepakbarwal98@gmail.com>
2023-12-24 18:24:53 +0530

    first version

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+console.log(`
+Deepak Barwal
+
+A passionate CS Engineer from India.
+Interested in FullStack Development and System Design.
+
+🐧 Twitter: https://twitter.com/__aizen_sama
+📖 Github: https://github.com/DeepakBarwal
+😎 LinkedIn: https://www.linkedin.com/in/d-barwal/
+
+`)
